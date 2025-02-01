228909b4bd5d5c2df887199e599acf00b1b435c2
Milind Sangale <smilind900@gmail.com>
2025-02-02 00:19:38 +0530

    javascipt commit

---
+++ Javascript/app.js
@@ -0,0 +1,91 @@
+/* console.log('Hello World!');
+console.log('Hello World!');
+console.log('Hello World!'); */
+/* let a = 10;
+let b = 20;
+console.log("sum is :",a + b);
+ */
+
+
+/* let pen = 10;
+let pencil = 20; */
+/* console.log("Total Price is  :",pen + pencil); */
+/* let milind = "Total price is :" + (pen + pencil)+ " ruppes"; */
+/* console.log(milind); */
+/* let sumall =`Total price is : ${pen + pencil} ruppes`;
+console.log(sumall);
+console.log(`Total price is : ${pen + pencil} ruppes`;); */
+
+
+// Arithmatic operators
+/* let a=10;
+let b=20;
+console.log("sum is :",a+b);
+console.log("sub is :",a-b);
+console.log("mul is :",a*b);
+console.log("div is :",a/b);
+
+console.log(a++);
+console.log(++a); */
+
+
+//Assignment operators
+/* let x=10;
+let y=20;
+x=y;
+console.log(x);
+ */
+
+ //Comparison operators
+/* 
+age =21;
+console.log("age is :",age<20);
+console.log("age is :",age>20);
+console.log("age is :",age==21);
+console.log("age is :",age>=20);
+console.log("age is :",age<=20); 
+
+
+
+ */
+
+/* 
+
+console.log("before my if statement");
+let age = 17;
+if(age>18){
+    console.log("you are eligible to vote");
+}
+else{
+    console.log("you are not eligible to vote");
+}
+console.log("after my if statement");
+
+ */
+
+/* let colour = red;
+if(colour=="red"){
+    console.log("stop the vehicle");
+}
+
+
+ */
+
+/* console.log("before my if statement");
+
+ */
+
+
+let size = "XL";
+
+if(size === "XL"){
+    console.log("Price is Rs.250");
+}else if(size === "L") {
+    console.log("Price is Rs.200");
+}else if(size === "M") {
+    console.log("Price is Rs.100");
+} else{
+    console.log("Price is Rs.50");
+}
+
+
