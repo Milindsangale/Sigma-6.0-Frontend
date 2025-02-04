35669de649d33babfc85630e1adaeae264768870
Milind Sangale <smilind900@gmail.com>
2025-02-04 20:53:34 +0530

    javascript

---
+++ Javascript/Part 3/app.js
@@ -0,0 +1,32 @@
+/* 
+
+ let password = prompt('Enter the password');
+ let passwordLength = password.trim();
+ console.log(passwordLength); */
+
+
+
+let str = "milind sangale"
+console.log(str.toLocaleUpperCase());
+
+
+
+let milind = "MILIUND SANGALE";
+console.log(milind.toLocaleLowerCase());
+
+
+
+let sangale = "Milind";
+sangale.indexOf('i');
+console.log(sangale.indexOf('i'));
+
+
+let index ="           milind";
+console.log("before :",index);
+/* let index1 = index.trim();
+console.log("after :",index1);
+let index2 = index1.toLocaleUpperCase();
+console.log("after upper case :",index2); */
+
+let newindex = index.trim().toLocaleUpperCase();
+console.log(newindex);
