52a5c1a1b2c4c1d2e5c9a51d4bc0275f175f9f5c
Milind Sangale <smilind900@gmail.com>
2025-02-15 11:12:28 +0530

    javascript

--- Javascript/part 7/app.js
+++ Javascript/part 7/app.js
@@ -1,7 +1,7 @@
+/* console.log("milindsangale");
 console.log("milindsangale");
-console.log("milindsangale");
-
-try{
+ */
+/* try{
     console.log(a);
 } catch(milind){
     console.log("djanfjj");
@@ -15,3 +15,52 @@ console.log("milindsangale");
 console.log("milindsangale");
 console.log("milindsangale");
 
+ */
+
+/* const sum =(a,b) => {
+    console.log("addition a & b is",a+b)
+}
+
+ */
+
+/* const cube = a => {
+    return a*a*a;
+}
+
+
+const square = (a) => {
+    return a*a;
+} */
+
+
+
+/* console.log("Milind Sangale");
+
+setTimeout(() => {
+    console.log("kjhsjhghsk ");
+
+    
+}, 4000); */
+
+
+/* 
+let id=setInterval(() => {
+    console.log("milinds");
+}, 2000); */
+/* 
+clearInterval(id);
+ */
+
+const square = (n) => {
+    return n*n;
+
+}
+
+ let id = setInterval(() => {
+    console.log("hellow word");
+ }, 2000);
+
+
+setTimeout(() => {
+    clearInterval(id);
+}, 10000);
---
+++ Javascript/part 8/app.js
@@ -0,0 +1,16 @@
+console.log("part 8");
+
+
+let arr = [1,2,3,4,5];
+
+
+arr.forEach(function (el) {
+    console.log(el);
+});
+
+ let print = function(el) {
+    console.log(el);
+
+};
+arr.forEach(print); 
+ 
