fd55c5d8ee7b3f588930bbaae5e260c793802dfd
Milind Sangale <smilind900@gmail.com>
2025-02-16 22:58:12 +0530

    javascript

--- Javascript/part 8/app.js
+++ Javascript/part 8/app.js
@@ -14,3 +14,31 @@ arr.forEach(function (el) {
 };
 arr.forEach(print); 
  
+
+
+let every =[1,2,3,4].every((el) => (el%2==0));
+
+
+let every1 =[2,4].every((el) => (el%2==0));
+
+
+
+
+let every12 =[2,4].some((el) => (el%2==0));
+
+
+
+
+ let nums = [1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,
+            1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,
+            1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4,1,2,3,4
+             ];
+let newnumber = nums.reduce((res,el) => res+el);
+console.log(newnumber);
+
+
+function milindsangale(a,b) {
+    return a+b;
+    
+}
+let xxx=milind
