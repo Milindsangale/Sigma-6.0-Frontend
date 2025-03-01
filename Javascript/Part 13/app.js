b4f72ce0d13e07c048f76d83830a39239de6bec2
Milind Sangale <smilind900@gmail.com>
2025-03-01 22:29:55 +0530

    JAVASCRIPT

---
+++ Javascript/Part 13/app.js
@@ -0,0 +1,14 @@
+let h1 = document.querySelector('h1');
+h1.style.color = 'red';
+h1.innerText = 'Hello World';
+
+h1.style.fontSize = '50px';
+h1.style.fontFamily = 'Arial';
+h1.style.backgroundColor = 'yellow';
+h1.style.padding = '20px';
+h1.style.border = '2px solid green';
+h1.style.borderRadius = '10px';
+h1.style.textAlign = 'center';
+h1.style.boxShadow = '10px 10px 10px rgba(0, 0, 0, 0.5)';
+
+l
